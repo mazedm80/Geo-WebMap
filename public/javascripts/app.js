840b35c1d8b3c7ff58a5217533f84491515cf42b
mazedm80 <36367243+mazedm80@users.noreply.github.com>
2021-07-13 13:07:34 +0200

    added info control and legend control to the map

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -8,6 +8,66 @@ var mymap = L.map('mapid', {
     })
     .setView([21.21603208, 92.15620182], 13);
 
+// Adding fullscrean control
+mymap.on('fullscreenchange', function() {
+    if (mymap.isFullscreen()) {
+        console.log('entered fullscreen');
+    } else {
+        console.log('exited fullscreen');
+    }
+});
+// Addind info control
+var info = L.control();
+
+info.onAdd = function(map) {
+    this._div = L.DomUtil.create('div', 'info'); // create a div with a class "info"
+    this.update();
+    return this._div;
+};
+
+// method that we will use to update the control based on feature properties passed
+info.update = function(props) {
+    this._div.innerHTML = '<h4>Refugees Population by Camp</h4>' + (props ?
+        '<b>' + props.name + '</b><br/>Upazila: ' + props.upazila + ' | Union: ' + props.union + '</b><br/> Total Individuals: ' + props.population :
+        'Hover over a camp');
+};
+
+info.addTo(mymap);
+// Adding legend control to the map
+var legend = L.control({ position: 'bottomright' });
+
+legend.onAdd = function(map) {
+    var div = L.DomUtil.create('div', 'info legend');
+    div.innerHTML = '<h5>Legend</h5>' +
+        '<p><i style="background:#582767"></i>Camps</p>' +
+        '<p><i style="background:#DA426C"></i>Roads</p>' +
+        '<p><i style="background:#235FD9"></i>District Boundery</p>';
+    return div;
+};
+
+legend.addTo(mymap);
+// Function for heighlited feature
+function highlightFeature(e) {
+    var layer = e.target;
+
+    layer.setStyle({
+        stroke: false,
+        fillColor: '#038C33',
+        fillOpacity: 1
+    });
+    info.update(layer);
+    // console.log(layer.name);
+}
+// mouse out function
+function resetHighlight(e) {
+    var layer = e.target;
+    layer.setStyle({
+        stroke: false,
+        fillColor: '#582767',
+        fillOpacity: 0.7,
+    });
+    info.update();
+}
 // create a new pane with z index for layer overlay indexing
 mymap.createPane('circles');
 mymap.getPane('circles').style.zIndex = 650;
@@ -17,11 +77,9 @@ mymap.getPane('back').style.zIndex = 200;
 // create a new pane with z index for layer overlay indexing
 mymap.createPane('farback');
 mymap.getPane('farback').style.zIndex = 100;
-
-// L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png')
-//     .addTo(mymap);
+// adding basemap layer by mapbox api
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-    // attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     opacity: 0.70,
     id: 'mapbox/streets-v11',
@@ -30,18 +88,7 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
     pane: 'farback',
     accessToken: 'pk.eyJ1Ijoic21hcnF1ZXMiLCJhIjoiY2pyeGg2M2ZlMGxhNjQ0b2E1ajh5OWl5YiJ9.G074wzH9zGbDUonl3rJWrg'
 }).addTo(mymap);
-
-// $.get("http://localhost:3000/v1/api/fploc", function(data, status) {
-//         L.geoJSON(data, {
-//                 onEachFeature: function(feature, layer) {
-//                     layer.bindPopup(feature.properties.Location);
-//                 }
-//             })
-//             .addTo(mymap)
-//     },
-//     dataType = "json"
-// );
-
+// jQuery ajax api call for adding foothpath multiline featureclass
 $.get("/v1/api/fploc", function(data, status) {
         L.geoJSON(data, {
                 style: function(feature) {
@@ -58,7 +105,7 @@ $.get("/v1/api/fploc", function(data, status) {
     },
     dataType = "json"
 );
-
+// jQuery ajax api call for adding area of interest polygon
 $.get("/v1/api/aoi", function(data, status) {
         L.geoJSON(data, {
                 style: function(feature) {
@@ -66,7 +113,7 @@ $.get("/v1/api/aoi", function(data, status) {
                         stroke: true,
                         weight: 2,
                         // opacity: 0.5,
-                        // color: '#DA426C'
+                        color: '#235FD9'
                     };
                 },
                 pane: 'back'
@@ -75,27 +122,27 @@ $.get("/v1/api/aoi", function(data, status) {
     },
     dataType = "json"
 );
-// #582767, #272C8C
-$.get("/v1/api/camploc", function(data, status) {
-        data.forEach(element => {
-            circle = new L.circle([element.Latitude, element.Longitude], {
-                    stroke: false,
-                    fillColor: '#582767',
-                    fillOpacity: 0.7,
-                    radius: parseInt(element.Total_HH) / 100
-                }, { pane: 'circles' })
-                .bringToFront()
-                .bindPopup(element.New_Camp_Name)
-                .addTo(mymap);
-        });
-    },
-    dataType = "json"
-);
-
-mymap.on('fullscreenchange', function() {
-    if (mymap.isFullscreen()) {
-        console.log('entered fullscreen');
-    } else {
-        console.log('exited fullscreen');
-    }
+// jQuery ajax api call for adding camp location
+var camploc = $.parseJSON($.ajax({
+    url: '/v1/api/camploc',
+    dataType: "json",
+    async: false
+}).responseText);
+// Drawing camp location circle to the map
+camploc.forEach(element => {
+    var circleLyr = L.circle([element.Latitude, element.Longitude], {
+            stroke: false,
+            fillColor: '#582767',
+            fillOpacity: 0.7,
+            radius: parseInt(element.Total_HH) / 50
+        }, { pane: 'circles' })
+        .addTo(mymap);
+    // Adding properties to the layer
+    circleLyr.name = element.New_Camp_Name;
+    circleLyr.upazila = element.Upazila;
+    circleLyr.union = element.Union;
+    circleLyr.population = element.Total_Individuals;
+    circleLyr.on('mouseover', highlightFeature);
+    circleLyr.on('mouseout', resetHighlight);
+    circleLyr.addTo(mymap);
 });
